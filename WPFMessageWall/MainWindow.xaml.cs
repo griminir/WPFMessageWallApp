755d75c3e63b9c89f1b7a60ff0761b93a7bd0cc6
viktor elliot Degray <viktor.elliot.king@gmail.com>
2024-06-09 03:20:18 +0200

    Add project files.

---
+++ WPFMessageWall/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFMessageWall
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        BindingList<string> _messages = new BindingList<string>();
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            MessageList.ItemsSource = _messages;
+        }
+
+        private void AddMessage_Click(object sender, RoutedEventArgs e)
+        {
+            _messages.Add(MessageText.Text);
+            MessageText.Text = "";
+        }
+    }
+}
